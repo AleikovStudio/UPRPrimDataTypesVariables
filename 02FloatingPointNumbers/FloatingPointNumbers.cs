e39b0a526e8dbc6ce793184845006fdfa360ebcb
Aleikov Studio <golearnweb@gmail.com>
2015-10-25 21:05:34 +0200

    First commit

---
+++ 02FloatingPointNumbers/FloatingPointNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class FloatingPointNumbers
+{
+    static void Main()
+    {
+        decimal a = 3.141592653589793238M;
+        double b = 1.60217657;
+        decimal c = 7.8184261974584555216535342341M;
+
+        Console.WriteLine("{0}\n{1}\n{2}", a, b, c);
+    }
+}
